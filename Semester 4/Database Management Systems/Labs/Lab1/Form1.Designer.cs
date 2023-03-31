26c8e9e964394239bff814da479917373328c9f4
913-Horvath-Krisztina <91715582+krisztinahorvath@users.noreply.github.com>
2023-03-31 14:04:52 +0300

    add dbms lab1

---
+++ Semester 4/Database Management Systems/Labs/Lab1/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿namespace DBMSLab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewTeaTypes = new DataGridView();
+            DataGridViewTeas = new DataGridView();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBoxTeaId = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            textBoxTeaTypeId = new TextBox();
+            textBoxName = new TextBox();
+            textBoxQuantity = new TextBox();
+            textBoxPrice = new TextBox();
+            textBoxDistributorId = new TextBox();
+            buttonAdd = new Button();
+            buttonRemove = new Button();
+            buttonUpdate = new Button();
+            ((System.ComponentModel.ISupportInitialize)DataGridViewTeaTypes).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)DataGridViewTeas).BeginInit();
+            SuspendLayout();
+            // 
+            // DataGridViewTeaTypes
+            // 
+            DataGridViewTeaTypes.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DataGridViewTeaTypes.Location = new Point(22, 41);
+            DataGridViewTeaTypes.Name = "DataGridViewTeaTypes";
+            DataGridViewTeaTypes.RowHeadersWidth = 62;
+            DataGridViewTeaTypes.RowTemplate.Height = 33;
+            DataGridViewTeaTypes.SelectionMode = DataGridViewSelectionMode.ColumnHeaderSelect;
+            DataGridViewTeaTypes.Size = new Size(426, 225);
+            DataGridViewTeaTypes.TabIndex = 0;
+            DataGridViewTeaTypes.SelectionChanged += DataGridViewTeaTypes_SelectionChanged;
+            // 
+            // DataGridViewTeas
+            // 
+            DataGridViewTeas.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            DataGridViewTeas.Location = new Point(22, 311);
+            DataGridViewTeas.Name = "DataGridViewTeas";
+            DataGridViewTeas.RowHeadersWidth = 62;
+            DataGridViewTeas.RowTemplate.Height = 33;
+            DataGridViewTeas.Size = new Size(426, 225);
+            DataGridViewTeas.TabIndex = 1;
+            DataGridViewTeas.SelectionChanged += DataGridViewTeas_SelectionChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(22, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(82, 25);
+            label1.TabIndex = 2;
+            label1.Text = "TeaTypes";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(22, 283);
+            label2.Name = "label2";
+            label2.Size = new Size(45, 25);
+            label2.TabIndex = 3;
+            label2.Text = "Teas";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(565, 41);
+            label3.Name = "label3";
+            label3.Size = new Size(53, 25);
+            label3.TabIndex = 4;
+            label3.Text = "TeaId";
+            // 
+            // textBoxTeaId
+            // 
+            textBoxTeaId.Location = new Point(733, 41);
+            textBoxTeaId.Name = "textBoxTeaId";
+            textBoxTeaId.Size = new Size(150, 31);
+            textBoxTeaId.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(565, 111);
+            label4.Name = "label4";
+            label4.Size = new Size(90, 25);
+            label4.TabIndex = 6;
+            label4.Text = "TeaTypeId";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(565, 176);
+            label5.Name = "label5";
+            label5.Size = new Size(59, 25);
+            label5.TabIndex = 7;
+            label5.Text = "Name";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(565, 241);
+            label6.Name = "label6";
+            label6.Size = new Size(80, 25);
+            label6.TabIndex = 8;
+            label6.Text = "Quantity";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(565, 311);
+            label7.Name = "label7";
+            label7.Size = new Size(49, 25);
+            label7.TabIndex = 9;
+            label7.Text = "Price";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(565, 376);
+            label8.Name = "label8";
+            label8.Size = new Size(113, 25);
+            label8.TabIndex = 10;
+            label8.Text = "DistributorId";
+            // 
+            // textBoxTeaTypeId
+            // 
+            textBoxTeaTypeId.Location = new Point(733, 111);
+            textBoxTeaTypeId.Name = "textBoxTeaTypeId";
+            textBoxTeaTypeId.Size = new Size(150, 31);
+            textBoxTeaTypeId.TabIndex = 11;
+            // 
+            // textBoxName
+            // 
+            textBoxName.Location = new Point(733, 176);
+            textBoxName.Name = "textBoxName";
+            textBoxName.Size = new Size(150, 31);
+            textBoxName.TabIndex = 12;
+            // 
+            // textBoxQuantity
+            // 
+            textBoxQuantity.Location = new Point(733, 241);
+            textBoxQuantity.Name = "textBoxQuantity";
+            textBoxQuantity.Size = new Size(150, 31);
+            textBoxQuantity.TabIndex = 13;
+            // 
+            // textBoxPrice
+            // 
+            textBoxPrice.Location = new Point(733, 311);
+            textBoxPrice.Name = "textBoxPrice";
+            textBoxPrice.Size = new Size(150, 31);
+            textBoxPrice.TabIndex = 14;
+            // 
+            // textBoxDistributorId
+            // 
+            textBoxDistributorId.Location = new Point(733, 373);
+            textBoxDistributorId.Name = "textBoxDistributorId";
+            textBoxDistributorId.Size = new Size(150, 31);
+            textBoxDistributorId.TabIndex = 15;
+            // 
+            // buttonAdd
+            // 
+            buttonAdd.Location = new Point(555, 435);
+            buttonAdd.Name = "buttonAdd";
+            buttonAdd.Size = new Size(112, 34);
+            buttonAdd.TabIndex = 16;
+            buttonAdd.Text = "add";
+            buttonAdd.UseVisualStyleBackColor = true;
+            buttonAdd.Click += buttonAdd_Click;
+            // 
+            // buttonRemove
+            // 
+            buttonRemove.Location = new Point(702, 435);
+            buttonRemove.Name = "buttonRemove";
+            buttonRemove.Size = new Size(112, 34);
+            buttonRemove.TabIndex = 17;
+            buttonRemove.Text = "remove";
+            buttonRemove.UseVisualStyleBackColor = true;
+            buttonRemove.Click += buttonRemove_Click;
+            // 
+            // buttonUpdate
+            // 
+            buttonUpdate.Location = new Point(853, 435);
+            buttonUpdate.Name = "buttonUpdate";
+            buttonUpdate.Size = new Size(112, 34);
+            buttonUpdate.TabIndex = 18;
+            buttonUpdate.Text = "update";
+            buttonUpdate.UseVisualStyleBackColor = true;
+            buttonUpdate.Click += buttonUpdate_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(986, 548);
+            Controls.Add(buttonUpdate);
+            Controls.Add(buttonRemove);
+            Controls.Add(buttonAdd);
+            Controls.Add(textBoxDistributorId);
+            Controls.Add(textBoxPrice);
+            Controls.Add(textBoxQuantity);
+            Controls.Add(textBoxName);
+            Controls.Add(textBoxTeaTypeId);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(textBoxTeaId);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(DataGridViewTeas);
+            Controls.Add(DataGridViewTeaTypes);
+            Name = "Form1";
+            Text = "TeaShop";
+            Load += Form1_Load_1;
+            ((System.ComponentModel.ISupportInitialize)DataGridViewTeaTypes).EndInit();
+            ((System.ComponentModel.ISupportInitialize)DataGridViewTeas).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        private void DataGridViewTeas_SelectionChanged1(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        private DataGridView DataGridViewTeaTypes;
+        private DataGridView DataGridViewTeas;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBoxTeaId;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private TextBox textBoxTeaTypeId;
+        private TextBox textBoxName;
+        private TextBox textBoxQuantity;
+        private TextBox textBoxPrice;
+        private TextBox textBoxDistributorId;
+        private Button buttonAdd;
+        private Button buttonRemove;
+        private Button buttonUpdate;
+    }
+}
---
+++ Semester 4/Database Management Systems/Labs/Lab1/Form1.cs
@@ -0,0 +1,376 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Globalization;
+
+
+namespace DBMSLab1
+{
+    public partial class Form1 : Form
+    {
+
+        private SqlConnection dbConnection;
+        private SqlDataAdapter daTeas, daTeaTypes;
+        private DataSet tableData;
+        private DataRelation drTeaTypeTeas;
+        BindingSource bsTeas, bsTeaTypes;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void ReloadTeasTableView()
+        {
+            if (tableData.Tables["Teas"] != null)
+            {
+                tableData.Tables["Teas"].Clear();
+            }
+            daTeas.Fill(tableData, "Teas");
+            DataGridViewTeas.DataSource = bsTeas;
+        }
+
+        private void DataGridViewTeaTypes_SelectionChanged(object sender, EventArgs e)
+        {
+            textBoxTeaId.Clear();
+            textBoxTeaTypeId.Clear();
+            textBoxName.Clear();
+            textBoxQuantity.Clear();
+            textBoxPrice.Clear();
+            textBoxDistributorId.Clear();
+
+            if (DataGridViewTeaTypes.SelectedRows.Count != 0)
+            {
+                DataGridViewRow selectedRow = DataGridViewTeaTypes.SelectedRows[0];
+                daTeas.SelectCommand = new SqlCommand("SELECT * FROM Teas WHERE ttid = " + selectedRow.Cells[0].Value, dbConnection);
+
+                ReloadTeasTableView();
+            }
+        }
+
+        private void DataGridViewTeas_SelectionChanged(object sender, EventArgs e)
+        {
+            if (DataGridViewTeas.SelectedRows.Count != 0)
+            {
+                DataGridViewRow selectedRow = DataGridViewTeas.SelectedRows[0];
+
+                textBoxTeaId.Text = selectedRow.Cells[0].Value.ToString();
+                textBoxTeaTypeId.Text = selectedRow.Cells[1].Value.ToString();
+                textBoxName.Text = selectedRow.Cells[2].Value.ToString();
+                textBoxQuantity.Text = selectedRow.Cells[3].Value.ToString();
+                textBoxPrice.Text = selectedRow.Cells[4].Value.ToString();
+                textBoxDistributorId.Text = selectedRow.Cells[5].Value.ToString();
+
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            dbConnection = new SqlConnection("Server=DESKTOP-1AIPMIG\\SQLEXPRESS;Database=TeaShop;Trusted_Connection=True");
+            dbConnection.Open();
+
+            /// TeaTypes
+            daTeaTypes = new SqlDataAdapter("SELECT * FROM TeaTypes", dbConnection);
+            tableData = new DataSet();
+            daTeaTypes.Fill(tableData, "TeaTypes");
+            DataGridViewTeaTypes.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+
+            /// Teas
+            daTeas = new SqlDataAdapter("SELECT * FROM Teas", dbConnection);
+            daTeas.Fill(tableData, "Teas");
+            DataGridViewTeas.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+
+            DataColumn teaTypeIdTeaType = tableData.Tables["TeaTypes"].Columns["ttid"];
+            DataColumn teaTypeIdTea = tableData.Tables["Teas"].Columns["ttid"];
+            drTeaTypeTeas = new DataRelation("FK_TeaType_Tea", teaTypeIdTeaType, teaTypeIdTea);
+            tableData.Relations.Add(drTeaTypeTeas);
+
+            bsTeaTypes = new BindingSource();
+            bsTeaTypes.DataSource = tableData;
+            bsTeaTypes.DataMember = "TeaTypes";
+
+            bsTeas = new BindingSource();
+            bsTeas.DataSource = bsTeaTypes;
+            bsTeas.DataMember = "FK_TeaType_Tea";
+            DataGridViewTeaTypes.DataSource = bsTeaTypes;
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand("INSERT INTO Teas (ttid, name, quantity, price, did)" +
+                "VALUES (@ttid, @name, @quantity, @price, @did)", dbConnection);
+
+            if (textBoxTeaId.Text.Length != 0)
+            {
+                MessageBox.Show("The TeaId field should be empty!");
+            }
+            else
+            {
+                try
+                {
+                    if (textBoxTeaTypeId.Text.Length != 0)
+                    {
+                        int ttid = Int32.Parse(textBoxTeaTypeId.Text);
+
+
+                        int quantity;
+                        if (textBoxQuantity.Text.Length != 0)
+                            quantity = Int32.Parse(textBoxQuantity.Text);
+                        else
+                        {
+                            MessageBox.Show("Please provide a Quantity!");
+                            return;
+                        }
+
+                        float convertPrice;
+                        decimal price;
+                        if (textBoxPrice.Text.Length != 0)
+                        {
+                            convertPrice = float.Parse(textBoxPrice.Text);
+                            price = Math.Round((decimal)convertPrice, 2);
+                        }
+                        else price = 1;
+
+
+                        int did;
+                        if (textBoxDistributorId.Text.Length != 0)
+                            did = Int32.Parse(textBoxDistributorId.Text);
+                        else
+                        {
+                            MessageBox.Show("Please provide a DistributorId!");
+                            return;
+                        }
+
+
+                        if (textBoxName.Text.Length == 0)
+                        {
+                            MessageBox.Show("Please provide a Name!");
+                            return;
+                        }
+
+                        /// ttid
+                        command.Parameters.Add("@ttid", SqlDbType.Int);
+                        command.Parameters["@ttid"].Value = ttid;
+
+                        /// name 
+                        command.Parameters.Add("@name", SqlDbType.VarChar, 40);
+                        command.Parameters["@name"].Value = textBoxName.Text;
+
+                        /// quantity
+                        command.Parameters.Add("@quantity", SqlDbType.Int);
+                        command.Parameters["@quantity"].Value = quantity;
+
+                        /// price
+                        command.Parameters.Add("@price", SqlDbType.Float);
+                        command.Parameters["@price"].Value = price;
+
+                        /// did
+                        command.Parameters.Add("@did", SqlDbType.Int);
+                        command.Parameters["@did"].Value = did;
+
+                        try
+                        {
+                            daTeas.InsertCommand = command;
+                            daTeas.InsertCommand.ExecuteNonQuery();
+
+                            ReloadTeasTableView();
+
+                            MessageBox.Show("Add executed successfully!");
+                        }
+                        catch (SqlException ex)
+                        {
+                            MessageBox.Show(ex.Message);
+                        }
+
+                        
+                    }
+                    else
+                        MessageBox.Show("Please provide a TeaTypeId!");
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void buttonRemove_Click(object sender, EventArgs e)
+        {
+            SqlCommand command = new SqlCommand("DELETE FROM Teas where tid = @tid", dbConnection);
+
+            if (textBoxTeaId.Text.Length == 0)
+            {
+                MessageBox.Show("Please provide a TeaId!");
+            }
+            else
+            {
+                int tid = Int32.Parse(textBoxTeaId.Text);
+
+                command.Parameters.Add("@tid", SqlDbType.Int);
+                command.Parameters["@tid"].Value = tid;
+
+                try
+                {
+                    daTeas.DeleteCommand = command;
+                    int nrOfDeletedTeas = daTeas.DeleteCommand.ExecuteNonQuery();
+                    if (nrOfDeletedTeas == 0)
+                        MessageBox.Show("There is no Tea with the given id!");
+                    else
+                    {
+                        ReloadTeasTableView();
+                        MessageBox.Show("Remove executed successfully!");
+                    }
+                        
+
+                }
+                catch (SqlException ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+
+            SqlCommand command = new SqlCommand("UPDATE Teas SET ttid = @ttid, name = @name, " +
+                "quantity = @quantity, price = @price, did = @did " +
+                "WHERE tid = @tid", dbConnection);
+
+            try
+            {
+                if(textBoxTeaId.Text.Length == 0)
+                {
+                    MessageBox.Show("Please provide a TeaId!");
+                    return;
+                }
+
+                if (textBoxTeaTypeId.Text.Length != 0)
+                {
+                    int ttid = Int32.Parse(textBoxTeaTypeId.Text);
+
+                    int tid = Int32.Parse(textBoxTeaId.Text);
+
+                    int quantity;
+                    if (textBoxQuantity.Text.Length != 0)
+                        quantity = Int32.Parse(textBoxQuantity.Text);
+                    else
+                    {
+                        MessageBox.Show("Please provide a Quantity!");
+                        return;
+                    }
+
+                    float convertPrice;
+                    decimal price;
+                    if (textBoxPrice.Text.Length != 0)
+                    {
+                        convertPrice = float.Parse(textBoxPrice.Text);
+                        price = Math.Round((decimal)convertPrice, 2);
+                    }
+                    else price = 1;
+
+
+                    int did;
+                    if (textBoxDistributorId.Text.Length != 0)
+                        did = Int32.Parse(textBoxDistributorId.Text);
+                    else
+                    {
+                        MessageBox.Show("Please provide a DistributorId!");
+                        return;
+                    }
+
+
+                    if (textBoxName.Text.Length == 0)
+                    {
+                        MessageBox.Show("Please provide a Name!");
+                        return;
+                    }
+
+
+                    /// tid
+                    command.Parameters.Add("@tid", SqlDbType.Int);
+                    command.Parameters["@tid"].Value = tid;
+
+                    /// ttid
+                    command.Parameters.Add("@ttid", SqlDbType.Int);
+                    command.Parameters["@ttid"].Value = ttid;
+
+                    /// name 
+                    command.Parameters.Add("@name", SqlDbType.VarChar, 40);
+                    command.Parameters["@name"].Value = textBoxName.Text;
+
+                    /// quantity
+                    command.Parameters.Add("@quantity", SqlDbType.Int);
+                    command.Parameters["@quantity"].Value = quantity;
+
+                    /// price
+                    command.Parameters.Add("@price", SqlDbType.Float);
+                    command.Parameters["@price"].Value = price;
+
+                    /// did
+                    command.Parameters.Add("@did", SqlDbType.Int);
+                    command.Parameters["@did"].Value = did;
+
+                    try
+                    {
+                        daTeas.UpdateCommand = command;
+                        int nrOfUpdatedTeas = daTeas.UpdateCommand.ExecuteNonQuery();
+
+                        if (nrOfUpdatedTeas == 0)
+                        {
+                            MessageBox.Show("There is no Tea with the given id!");
+                        }
+                        else
+                        {
+                            ReloadTeasTableView();
+                            MessageBox.Show("Update executed successfully!");
+                        }
+
+                    }
+                    catch (SqlException ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+
+                }
+                else
+                {
+                    MessageBox.Show("Please provide a TeaTypeId!");
+                }
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+            //SqlCommandBuilder builder = new SqlCommandBuilder(daTeas);
+            //daTeas.UpdateCommand = builder.GetUpdateCommand();
+
+            //try
+            //{
+            //    daTeas.Update(tableData, "Teas");
+            //    ReloadTeasTableView();
+            //    MessageBox.Show("Update executed successfully!");
+            //}
+            //catch(SqlException ex)
+            //{
+            //    if (ex.Number == 547)
+            //        MessageBox.Show("The TeaTypeId or the DistributorId is invalid!");
+            //    else
+            //        MessageBox.Show(ex.Message);
+            //}
+            
+        }
+    }
+}
